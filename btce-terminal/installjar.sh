39b0a6074f0fd0fe1f66a56a4aecdc81c151681e
Ales Pravdin <pravdin.ales@gmail.com>
2014-11-20 23:34:46 +0300

    gradle scripts added

--- btce-terminal/installjar.sh
+++ btce-terminal/installjar.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn install:install-file -Dfile=btceterminal.jar -DgroupId=by.groovycoin -DartifactId=btce-terminal -Dversion=0.9.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./build/libs/btce-terminal.jar -DgroupId=by.groovycoin -DartifactId=btce-terminal -Dversion=0.9.1 -Dpackaging=jar
