e9b5b0c092d573d3f6219c9d0af636165234dcd9
Ales Pravdin <pravdin.ales@gmail.com>
2014-11-25 09:18:25 +0300

    Build scripts improved

--- btce-terminal/src/examples/groovy/getinfo.groovy
+++ btce-terminal/src/examples/groovy/getinfo.groovy
@@ -3,9 +3,9 @@
 /**
  * Created by Ales Pravdin on 10/7/14.
  */
-
+@Grab('org.slf4j:slf4j-api:1.7.7')
 @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1')
-@Grab('by.groovycoin:btce-terminal:0.9.31')
+@Grab('by.groovycoin:btce-terminal:0.9.4g')
 
 import by.groovycoin.btce.terminal.TradeTerminal
 import by.groovycoin.btce.terminal.TradeTerminalManager
